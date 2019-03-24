99b8d5ff056bbbeecebb75d2799c29c6900ebcf9
lais.lopes <laisjkl@hotmail.com>
2019-03-24 09:40:49 -0300

    Codificação Classe EnderecoVM.

--- src/ViewModel/EnderecoVM.java
+++ src/ViewModel/EnderecoVM.java
@@ -11,4 +11,69 @@ package ViewModel;
  */
 public class EnderecoVM {
     
+    private String cep;
+    private String logradouro;
+    private int numero;
+    private String bairro;
+    private String cidade;
+    private String uf;
+    private String complemento;
+
+    public String getCep() {
+        return cep;
+    }
+
+    public void setCep(String cep) {
+        this.cep = cep;
+    }
+
+    public String getLogradouro() {
+        return logradouro;
+    }
+
+    public void setLogradouro(String logradouro) {
+        this.logradouro = logradouro;
+    }
+
+    public int getNumero() {
+        return numero;
+    }
+
+    public void setNumero(int numero) {
+        this.numero = numero;
+    }
+
+    public String getBairro() {
+        return bairro;
+    }
+
+    public void setBairro(String bairro) {
+        this.bairro = bairro;
+    }
+
+    public String getCidade() {
+        return cidade;
+    }
+
+    public void setCidade(String cidade) {
+        this.cidade = cidade;
+    }
+
+    public String getUf() {
+        return uf;
+    }
+
+    public void setUf(String uf) {
+        this.uf = uf;
+    }
+
+    public String getComplemento() {
+        return complemento;
+    }
+
+    public void setComplemento(String complemento) {
+        this.complemento = complemento;
+    }
+    
+    
 }
