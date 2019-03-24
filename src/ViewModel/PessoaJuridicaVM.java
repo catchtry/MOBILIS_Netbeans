b948786fd1312372e78d8835ec2f3a2a6fd81cbc
lais.lopes <laisjkl@hotmail.com>
2019-03-24 10:09:53 -0300

    Codificação classe PessoaJuridicaVM.

--- src/ViewModel/PessoaJuridicaVM.java
+++ src/ViewModel/PessoaJuridicaVM.java
@@ -11,4 +11,24 @@ package ViewModel;
  */
 public class PessoaJuridicaVM {
     
+    private String cnpj;
+    private InformacoesGeraisVM informacoesGerais;
+
+    public String getCnpj() {
+        return cnpj;
+    }
+
+    public void setCnpj(String cnpj) {
+        this.cnpj = cnpj;
+    }
+
+    public InformacoesGeraisVM getInformacoesGerais() {
+        return informacoesGerais;
+    }
+
+    public void setInformacoesGerais(InformacoesGeraisVM informacoesGerais) {
+        this.informacoesGerais = informacoesGerais;
+    }
+    
+    
 }
