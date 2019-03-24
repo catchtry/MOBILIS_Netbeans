d2f3205afb7540d0b9056205ee38fa9518da4726
lais.lopes <laisjkl@hotmail.com>
2019-03-24 09:57:36 -0300

    Codificação classe ContatoVM.

--- src/ViewModel/ContatoVM.java
+++ src/ViewModel/ContatoVM.java
@@ -11,4 +11,33 @@ package ViewModel;
  */
 public class ContatoVM {
     
+    private String telefone;
+    private String celular;
+    private String email;
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+    public String getCelular() {
+        return celular;
+    }
+
+    public void setCelular(String celular) {
+        this.celular = celular;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    
+    
 }
