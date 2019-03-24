ff91e63af936e139aff844f2321ea72c12934eef
lais.lopes <laisjkl@hotmail.com>
2019-03-24 10:11:07 -0300

    Codificação classe PessoaFisicaVM.

--- src/ViewModel/PessoaFisicaVM.java
+++ src/ViewModel/PessoaFisicaVM.java
@@ -11,4 +11,24 @@ package ViewModel;
  */
 public class PessoaFisicaVM {
     
+    private String cpf;
+    private InformacoesGeraisVM informacoesGerais;
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public InformacoesGeraisVM getInformacoesGerais() {
+        return informacoesGerais;
+    }
+
+    public void setInformacoesGerais(InformacoesGeraisVM informacoesGerais) {
+        this.informacoesGerais = informacoesGerais;
+    }
+    
+    
 }
