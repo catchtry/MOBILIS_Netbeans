14b2c0def635d4caf7f4edc73c2936737234c854
amandaqcarreiro <amandaqcarreiro@gmail.com>
2019-03-30 20:05:20 -0300

    adicionei o delete

--- src/DAL/PessoaFisicaDAO.java
+++ src/DAL/PessoaFisicaDAO.java
@@ -21,96 +21,117 @@ import java.util.logging.Logger;
  * @author Thiago
  */
 public class PessoaFisicaDAO {
-    
-    public void criar(PessoaFisicaVM pf){
+
+    public void criar(PessoaFisicaVM pf) {
         Connection con = ConexaoBanco.getConnection();
-        PreparedStatement stm = null; 
+        PreparedStatement stm = null;
         try {
             stm = con.prepareStatement("INSERT INTO pessoafisica (cpf, nome, logradouro, cep, numero, bairro, cidade, uf, complemento, telefone, email, celular) values(?,?,?,?,?,?,?,?,?,?,?,?)");
-            stm.setString(1,pf.getCpf());
-            stm.setString(2,pf.getNome());
-            stm.setString(3,pf.getLogradouro());
-            stm.setString(4,pf.getCep());
-            stm.setInt(5,pf.getNumero());
-            stm.setString(6,pf.getBairro());
-            stm.setString(7,pf.getCidade());
-            stm.setString(8,pf.getUf());
-            stm.setString(9,pf.getComplemento());
-            stm.setString(10,pf.getTelefone());
-            stm.setString(11,pf.getEmail());
-            stm.setString(12,pf.getCelular());
-            
+            stm.setString(1, pf.getCpf());
+            stm.setString(2, pf.getNome());
+            stm.setString(3, pf.getLogradouro());
+            stm.setString(4, pf.getCep());
+            stm.setInt(5, pf.getNumero());
+            stm.setString(6, pf.getBairro());
+            stm.setString(7, pf.getCidade());
+            stm.setString(8, pf.getUf());
+            stm.setString(9, pf.getComplemento());
+            stm.setString(10, pf.getTelefone());
+            stm.setString(11, pf.getEmail());
+            stm.setString(12, pf.getCelular());
+
             stm.executeUpdate();
             JOptionPane.showMessageDialog(null, "Salvo com sucesso");
         } catch (SQLException ex) {
-            JOptionPane.showMessageDialog(null, "Erro ao salvar: " +ex);
-        }finally{
+            JOptionPane.showMessageDialog(null, "Erro ao salvar: " + ex);
+        } finally {
             ConexaoBanco.closeConnection(con, stm);
         }
         
-        public List<PessoaFisicaVM> ler(){
-            Connection con = ConexaoBanco.getConnection();
-            PreparedStatement stm = null;
-            ResultSet rs = null;
         
-            List<PessoaFisicaVM> pessoasf = new ArrayList<>();
+
+    public List<PessoaFisicaVM> ler() {
+        Connection con = ConexaoBanco.getConnection();
+        PreparedStatement stm = null;
+        ResultSet rs = null;
+
+        List<PessoaFisicaVM> pessoasf = new ArrayList<>();
+
+        try {
+            stm = con.prepareStatement("SELECT * FROM pessoafisica");
+            rs = stm.executeQuery();
+            while (rs.next()) {
+
+                PessoaFisicaVM pessoaf = new PessoaFisicaVM();
+
+                pessoaf.setCpf(rs.getString("cpf"));
+                pessoaf.setNome(rs.getString("nome"));
+                pessoaf.setLogradouro(rs.getString("logradouro"));
+                pessoaf.setCep(rs.getString("cep"));
+                pessoaf.setNumero(rs.getInt("numero"));
+                pessoaf.setBairro(rs.getString("bairro"));
+                pessoaf.setCidade(rs.getString("cidade"));
+                pessoaf.setUf(rs.getString("uf"));
+                pessoaf.setComplemtento(rs.getString("complemento"));
+                pessoaf.setTelefone(rs.getString("telefone"));
+                pessoaf.setEmail(rs.getString("email"));
+                pessoaf.setCelular(rs.getString("celular"));
+                pessoasf.add(pessoaf);
+
+            }
+        } catch (SQLException ex) {
+            Logger.getLogger(VeiculoDAO.class.getName()).log(Level.SEVERE, null, ex);
+        } finally {
+            ConexaoBanco.closeConnection(con, stm, rs);
+        }
+        return pessoasf;
+    }        
+        
+
+    public void atualizar(PessoaFisicaVM pf) {
+        Connection con = ConexaoBanco.getConnection();
+        PreparedStatement stm = null;
+
+        try {
+            stm = con.prepareStatement("UPDATE SET pessoafisica cpf = ?, nome = ?, logradouro = ?, cep = ?, numero = ?, bairro = ?, cidade = ?, uf = ?, complemento = ?, telefone = ?, email = ?, celular =?) WHERE cpf = ?");
+            stm.setString(1, pf.getCpf());
+            stm.setString(2, pf.getNome());
+            stm.setString(3, pf.getLogradouro());
+            stm.setString(4, pf.getCep());
+            stm.setInt(5, pf.getNumero());
+            stm.setString(6, pf.getBairro());
+            stm.setString(7, pf.getCidade());
+            stm.setString(8, pf.getUf());
+            stm.setString(9, pf.getComplemento());
+            stm.setString(10, pf.getTelefone());
+            stm.setString(11, pf.getEmail());
+            stm.setString(12, pf.getCelular());
+
+            stm.executeUpdate();
+            JOptionPane.showMessageDialog(null, "Atualizado com sucesso");
+        } catch (SQLException ex) {
+            JOptionPane.showMessageDialog(null, "Erro ao atualizar: " + ex);
+        } finally {
+            ConexaoBanco.closeConnection(con, stm);
+        }
+    }
+    
+    public void remover(PessoaFisicaVM pf){    
+        Connection con = ConexaoBanco.getConnection();
+        PreparedStatement stm = null;
         
             try {
-                stm = con.prepareStatement("SELECT * FROM pessoafisica");
-                rs = stm.executeQuery();
-                while(rs.next()){
+                stm = con.prepareStatement("DELETE FROM pessoafisica WHERE cpf = ?");
+                stm.setString(1, pf.getPlaca());
 
-                    PessoaFisicaVM pessoaf = new PessoaFisicaVM();
-
-                    pessoaf.setCpf(rs.getString("cpf"));
-                    pessoaf.setNome(rs.getString("nome"));
-                    pessoaf.setLogradouro(rs.getString("logradouro"));
-                    pessoaf.setCep(rs.getString("cep"));
-                    pessoaf.setNumero(rs.getInt("numero"));
-                    pessoaf.setBairro(rs.getString("bairro"));
-                    pessoaf.setCidade(rs.getString("cidade"));
-                    pessoaf.setUf(rs.getString("uf"));
-                    pessoaf.setComplemtento(rs.getString("complemento"));
-                    pessoaf.setTelefone(rs.getString("telefone"));
-                    pessoaf.setEmail(rs.getString("email"));
-                    pessoaf.setCelular(rs.getString("celular"));
-                    pessoasf.add(pessoaf);
-
-                }
+                stm.executeUpdate();
+                JOptionPane.showMessageDialog(null, "Excluido com sucesso");
             } catch (SQLException ex) {
-                Logger.getLogger(VeiculoDAO.class.getName()).log(Level.SEVERE, null, ex);
+                JOptionPane.showMessageDialog(null, "Erro ao excluir: " +ex);
             }finally{
-                ConexaoBanco.closeConnection(con, stm, rs);
+                ConexaoBanco.closeConnection(con, stm);
             }
-            return pessoasf;
-            
-            public void atualizar(PessoaFisicaVM pf){
-                Connection con = ConexaoBanco.getConnection();
-                PreparedStatement stm = null; 
-                
-                try {
-                    stm = con.prepareStatement("UPDATE SET pessoafisica cpf = ?, nome = ?, logradouro = ?, cep = ?, numero = ?, bairro = ?, cidade = ?, uf = ?, complemento = ?, telefone = ?, email = ?, celular =?) WHERE codigoPf = ?");
-                    stm.setString(1,pf.getCpf());
-                    stm.setString(2,pf.getNome());
-                    stm.setString(3,pf.getLogradouro());
-                    stm.setString(4,pf.getCep());
-                    stm.setInt(5,pf.getNumero());
-                    stm.setString(6,pf.getBairro());
-                    stm.setString(7,pf.getCidade());
-                    stm.setString(8,pf.getUf());
-                    stm.setString(9,pf.getComplemento());
-                    stm.setString(10,pf.getTelefone());
-                    stm.setString(11,pf.getEmail());
-                    stm.setString(12,pf.getCelular());
-
-                    stm.executeUpdate();
-                    JOptionPane.showMessageDialog(null, "Atualizado com sucesso");
-                } catch (SQLException ex) {
-                    JOptionPane.showMessageDialog(null, "Erro ao atualizar: " +ex);
-                }finally{
-                    ConexaoBanco.closeConnection(con, stm);
-                }
-            }
-           
-}
         
+    }
+
+}
