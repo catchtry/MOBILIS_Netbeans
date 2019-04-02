b022542fd7114ebcbac87f61977f32ce6dab810f
lais.lopes <laisjkl@hotmail.com>
2019-04-01 21:18:56 -0300

    Refatoração botões tela Cliente View.

--- src/view/ClienteView.java
+++ src/view/ClienteView.java
@@ -23,7 +23,7 @@ public class ClienteView extends javax.swing.JFrame {
         btnEditarCliente_2 = new javax.swing.JButton();
         btnFiltrarCliente = new javax.swing.JButton();
         btnPesquisarCliente_2 = new javax.swing.JButton();
-        btnAdicionarCliente = new javax.swing.JButton();
+        btnCadastrarCliente_2 = new javax.swing.JButton();
         lblCliente = new javax.swing.JLabel();
         ClienteMenu = new javax.swing.JPanel();
         btnCadastrarCliente = new javax.swing.JButton();
@@ -42,30 +42,55 @@ public class ClienteView extends javax.swing.JFrame {
         btnDeletarCliente_2.setMaximumSize(new java.awt.Dimension(50, 50));
         btnDeletarCliente_2.setMinimumSize(new java.awt.Dimension(50, 50));
         btnDeletarCliente_2.setPreferredSize(new java.awt.Dimension(50, 50));
+        btnDeletarCliente_2.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btnDeletarCliente_2ActionPerformed(evt);
+            }
+        });
 
         btnEditarCliente_2.setIcon(new javax.swing.ImageIcon(getClass().getResource("/images/pencil.png"))); // NOI18N
         btnEditarCliente_2.setBorderPainted(false);
         btnEditarCliente_2.setMaximumSize(new java.awt.Dimension(50, 50));
         btnEditarCliente_2.setMinimumSize(new java.awt.Dimension(50, 50));
         btnEditarCliente_2.setPreferredSize(new java.awt.Dimension(50, 50));
+        btnEditarCliente_2.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btnEditarCliente_2ActionPerformed(evt);
+            }
+        });
 
         btnFiltrarCliente.setIcon(new javax.swing.ImageIcon(getClass().getResource("/images/filter.png"))); // NOI18N
         btnFiltrarCliente.setBorderPainted(false);
         btnFiltrarCliente.setMaximumSize(new java.awt.Dimension(50, 50));
         btnFiltrarCliente.setMinimumSize(new java.awt.Dimension(50, 50));
         btnFiltrarCliente.setPreferredSize(new java.awt.Dimension(50, 50));
+        btnFiltrarCliente.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btnFiltrarClienteActionPerformed(evt);
+            }
+        });
 
         btnPesquisarCliente_2.setIcon(new javax.swing.ImageIcon(getClass().getResource("/images/mag-glass.png"))); // NOI18N
         btnPesquisarCliente_2.setBorderPainted(false);
         btnPesquisarCliente_2.setMaximumSize(new java.awt.Dimension(50, 50));
         btnPesquisarCliente_2.setMinimumSize(new java.awt.Dimension(50, 50));
         btnPesquisarCliente_2.setPreferredSize(new java.awt.Dimension(50, 50));
+        btnPesquisarCliente_2.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btnPesquisarCliente_2ActionPerformed(evt);
+            }
+        });
 
-        btnAdicionarCliente.setIcon(new javax.swing.ImageIcon(getClass().getResource("/images/add.png"))); // NOI18N
-        btnAdicionarCliente.setBorderPainted(false);
-        btnAdicionarCliente.setMaximumSize(new java.awt.Dimension(50, 50));
-        btnAdicionarCliente.setMinimumSize(new java.awt.Dimension(50, 50));
-        btnAdicionarCliente.setPreferredSize(new java.awt.Dimension(50, 50));
+        btnCadastrarCliente_2.setIcon(new javax.swing.ImageIcon(getClass().getResource("/images/add.png"))); // NOI18N
+        btnCadastrarCliente_2.setBorderPainted(false);
+        btnCadastrarCliente_2.setMaximumSize(new java.awt.Dimension(50, 50));
+        btnCadastrarCliente_2.setMinimumSize(new java.awt.Dimension(50, 50));
+        btnCadastrarCliente_2.setPreferredSize(new java.awt.Dimension(50, 50));
+        btnCadastrarCliente_2.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btnCadastrarCliente_2ActionPerformed(evt);
+            }
+        });
 
         lblCliente.setFont(new java.awt.Font("Arial", 0, 36)); // NOI18N
         lblCliente.setText("Cliente");
@@ -78,7 +103,7 @@ public class ClienteView extends javax.swing.JFrame {
                 .addContainerGap()
                 .addComponent(lblCliente, javax.swing.GroupLayout.PREFERRED_SIZE, 169, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 275, Short.MAX_VALUE)
-                .addComponent(btnAdicionarCliente, javax.swing.GroupLayout.PREFERRED_SIZE, 50, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addComponent(btnCadastrarCliente_2, javax.swing.GroupLayout.PREFERRED_SIZE, 50, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addGap(18, 18, 18)
                 .addComponent(btnPesquisarCliente_2, javax.swing.GroupLayout.PREFERRED_SIZE, 50, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addGap(18, 18, 18)
@@ -95,7 +120,7 @@ public class ClienteView extends javax.swing.JFrame {
                 .addContainerGap()
                 .addGroup(ClienteBarraMenuLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
                     .addComponent(lblCliente, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                    .addComponent(btnAdicionarCliente, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                    .addComponent(btnCadastrarCliente_2, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
                     .addComponent(btnPesquisarCliente_2, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
                     .addComponent(btnFiltrarCliente, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
                     .addComponent(btnEditarCliente_2, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
@@ -117,10 +142,20 @@ public class ClienteView extends javax.swing.JFrame {
         btnPesquisarCliente.setFont(new java.awt.Font("Arial", 0, 18)); // NOI18N
         btnPesquisarCliente.setIcon(new javax.swing.ImageIcon(getClass().getResource("/images/mag-glass.png"))); // NOI18N
         btnPesquisarCliente.setText("Pesquisar");
+        btnPesquisarCliente.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btnPesquisarClienteActionPerformed(evt);
+            }
+        });
 
         btnDeletarCliente.setFont(new java.awt.Font("Arial", 0, 18)); // NOI18N
         btnDeletarCliente.setIcon(new javax.swing.ImageIcon(getClass().getResource("/images/trash.png"))); // NOI18N
         btnDeletarCliente.setText("Apagar");
+        btnDeletarCliente.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btnDeletarClienteActionPerformed(evt);
+            }
+        });
 
         btnEditarCliente.setFont(new java.awt.Font("Arial", 0, 18)); // NOI18N
         btnEditarCliente.setIcon(new javax.swing.ImageIcon(getClass().getResource("/images/pencil.png"))); // NOI18N
@@ -196,9 +231,34 @@ public class ClienteView extends javax.swing.JFrame {
         this.dispose();  
     }//GEN-LAST:event_btnEditarClienteActionPerformed
 
-    /**
-     * @param args the command line arguments
-     */
+    private void btnPesquisarClienteActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnPesquisarClienteActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_btnPesquisarClienteActionPerformed
+
+    private void btnDeletarClienteActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnDeletarClienteActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_btnDeletarClienteActionPerformed
+
+    private void btnCadastrarCliente_2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnCadastrarCliente_2ActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_btnCadastrarCliente_2ActionPerformed
+
+    private void btnPesquisarCliente_2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnPesquisarCliente_2ActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_btnPesquisarCliente_2ActionPerformed
+
+    private void btnFiltrarClienteActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnFiltrarClienteActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_btnFiltrarClienteActionPerformed
+
+    private void btnEditarCliente_2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnEditarCliente_2ActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_btnEditarCliente_2ActionPerformed
+
+    private void btnDeletarCliente_2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnDeletarCliente_2ActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_btnDeletarCliente_2ActionPerformed
+
     public static void main(String args[]) {
         /* Set the Nimbus look and feel */
         //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
@@ -249,8 +309,8 @@ public class ClienteView extends javax.swing.JFrame {
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JPanel ClienteBarraMenu;
     private javax.swing.JPanel ClienteMenu;
-    private javax.swing.JButton btnAdicionarCliente;
     private javax.swing.JButton btnCadastrarCliente;
+    private javax.swing.JButton btnCadastrarCliente_2;
     private javax.swing.JButton btnDeletarCliente;
     private javax.swing.JButton btnDeletarCliente_2;
     private javax.swing.JButton btnEditarCliente;
