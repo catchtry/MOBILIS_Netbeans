526ecd9693d91831526426eb8c260559e6edb771
lais.lopes <laisjkl@hotmail.com>
2019-05-03 21:24:18 -0300

    alterações para apresentação.

--- src/ViewModel/Cliente/ClienteFactoryVM.java
+++ src/ViewModel/Cliente/ClienteFactoryVM.java
@@ -5,7 +5,7 @@ public class ClienteFactoryVM {
     
     public IClienteVM instanciarClienteConformeOTipoDePessoa(TipoDePessoa tipoDePessoa, String cpf_cnpj) {
         if (tipoDePessoa == TipoDePessoa.FISICA) {
-            return new PessoaFisicaVM(cpf_cnpj);
+            return new PessoaFisicaVM(cpf_cnpj); 
         } else if (tipoDePessoa == TipoDePessoa.JURIDICA) {
             return new PessoaJuridicaVM(cpf_cnpj);
         } else {
